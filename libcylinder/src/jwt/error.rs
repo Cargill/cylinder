664f326826fdbdb85384f16c376e5cefc06059e3
Peter Schwarz <pschwarz@bitwise.io>
2020-10-27 14:26:42 -0500

    Implement JsonWebToken Builder and Parser
    This commit implements building and parsing of the custom cylinder JSON
    web token values.
    
    The process is divided between the JsonWebTokenBuilder, which is used to
    construct encoded, signed token.  As of this commit, the header values
    and claims values are a flat object of (String, String) pairs.
    
    The token is read and verified by the JsonWebTokenParser.  This parser
    wraps a Verifier instance which provides the signature verification.  It
    produces JsonWebToken struct that can be used to examine the token's
    claims.
    
    As of this commit, the algorithm choice for the JWT (the field "alg" in
    the header) is hard-coded to "secp256k1", as this is currently the only
    signing algorithm implemented in Cylinder.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

---
+++ libcylinder/src/jwt/error.rs
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2018-2020 Cargill Incorporated
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ------------------------------------------------------------------------------
+ */
+
+use std::error::Error;
+use std::fmt;
+
+/// An error that may occur while building the token.
+#[derive(Debug)]
+pub struct JsonWebTokenBuildError {
+    message: String,
+    source: Box<dyn Error>,
+}
+
+impl JsonWebTokenBuildError {
+    /// Construct a new error.
+    pub fn new<E: Into<Box<dyn Error>>>(message: String, source: E) -> Self {
+        Self {
+            message,
+            source: source.into(),
+        }
+    }
+}
+
+impl fmt::Display for JsonWebTokenBuildError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str(&self.message)
+    }
+}
+
+impl Error for JsonWebTokenBuildError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        Some(&*self.source)
+    }
+}
+
+/// A JWT string could not be parsed or validated.
+#[derive(Debug)]
+pub enum JsonWebTokenParseError {
+    InvalidToken(String),
+    InvalidSignature,
+}
+
+impl fmt::Display for JsonWebTokenParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            JsonWebTokenParseError::InvalidToken(msg) => f.write_str(&msg),
+            JsonWebTokenParseError::InvalidSignature => f.write_str("The signature was invalid"),
+        }
+    }
+}
+
+impl Error for JsonWebTokenParseError {}
